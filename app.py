411f4628ccfa346452c2e60fe7580853dfd66573
Rigvedsarougi <117500784+Rigvedsarougi@users.noreply.github.com>
2024-03-02 14:14:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from pandasai.llm.openai import OpenAI
 from dotenv import load_dotenv
 import os
-import yaml
+from pyYAML import yaml
 import pandas as pd
 from pandasai import PandasAI
 
